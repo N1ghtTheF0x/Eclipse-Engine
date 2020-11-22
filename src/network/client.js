b215280723e4f018498b6884ccea5261fe54e550
N1ghtTheF0x <60095145+N1ghtTheF0x@users.noreply.github.com>
2020-11-22 22:17:14 +0100

    added some networking stuff

--- src/network/client.js
+++ src/network/client.js
@@ -1,1 +1,19 @@
 const IOc = require("socket.io-client")
+const utils = require("./../utils")
+
+class EClient
+{
+    constructor(ip="localhost",port=2411)
+    {
+        this.ip = ip
+        this.port = port
+        this.address = this.ip+":"+this.port
+        this.client = IOc("http://"+this.ip+":"+this.port+"/")
+        utils.print("info","Created Client to Server "+this.ip+":"+this.port)
+    }
+    sendData(onEvent="data",data)
+    {
+        this.client.emit(onEvent,data)
+    }
+}
+module.exports = EClient
--- src/network/server.js
+++ src/network/server.js
@@ -1,1 +1,29 @@
 const serverC = require("socket.io")
+const utils = require("./../utils")
+
+class EServer
+{
+    constructor(port=2411)
+    {
+        this.server = new serverC.Server({path:"/",serveClient:true})
+        this.port = port
+        this.address4 = "0.0.0.0"
+        this.address6 = "::"
+        this.handlers = []
+        this.server.on("connect",function(socket=new serverC.Socket())
+        {
+            utils.print("info","A User has connected: "+socket.id)
+        })
+        utils.print("info","Created Server on Port "+this.port)
+    }
+    listen()
+    {
+        this.server.listen(this.port)
+        utils.print("info","Started listening")
+    }
+    broadcast()
+    {
+        this.server.emit()
+    }
+}
+module.exports = EServer
